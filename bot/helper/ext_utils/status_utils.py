791519b9a8ad9d19f3ddc44d705e6182764e31cc
5hojib <yesiamshojib@gmail.com>
2025-04-21 16:33:24 +0000

    format: auto-format code by ruff [skip actions].

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -298,7 +298,6 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
     button = buttons.build_menu(8)
     msg += f"<b>CPU:</b> {cpu_percent()}% | <b>FREE:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
     msg += f"\n<b>RAM:</b> {virtual_memory().percent}% | <b>UPTIME:</b> {get_readable_time(time() - bot_start_time)}"
-  
 
     # Add restart time if enabled
     if Config.AUTO_RESTART_ENABLED:
