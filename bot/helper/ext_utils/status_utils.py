d84a35622d54bf7c2c2507ff4f6e5774e325b750
AJisthebest2122 <anshjoshiisthebest@gmail.com>
2025-04-21 21:52:36 +0530

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -195,6 +195,7 @@ def source(self):
 
 
 async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=1):
+    "<blockquotes>"msg += "⚡Powered By Downloader Zone⚡\n\n""</blockquotes>"
     msg = ""
     msg += ""
     button = None
@@ -296,7 +297,6 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
             if status_value != status:
                 buttons.data_button(label, f"status {sid} st {status_value}")
     button = buttons.build_menu(8)
-    msg += "<b>    ⚡Powered By Downloader Zone⚡   </b>\n\n"
     msg += f"<b>CPU:</b> {cpu_percent()}% | <b>FREE:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
     msg += f"\n<b>RAM:</b> {virtual_memory().percent}% | <b>UPTIME:</b> {get_readable_time(time() - bot_start_time)}"
 
